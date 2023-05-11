9b7488fb5fde808897865adc5f1884b779e864d3
SudharsonRachandran <r.sudharson123@gmail.com>
2023-05-11 13:50:48 +0530

    first commit

---
+++ pages/Filter.py
@@ -0,0 +1,135 @@
+pipimport base64
+import pandas as pd
+import streamlit as st
+from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
+
+st.set_page_config(layout="wide")
+
+@st.cache_data
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+
+img = get_img_as_base64("image.jpg")
+
+page_bg_img = f"""
+<style>
+[data-testid="stAppViewContainer"] > .main {{
+background-image: url("https://southburlingtonlibrary.org/assets/image-cache/Teens/genes%20in%20a%20bottle.5d18602e.jpg");
+background-size: 120%;
+background-position: top left;
+background-repeat: no-repeat;
+background-attachment: scroll;
+}}
+[data-testid="stHeader"] {{
+background: rgba(0,0,0,0);
+}}
+[data-testid="stToolbar"] {{
+right: 2rem;
+}}
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+# URL of the image from the web
+image_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREh-y7VJtrA03RIlxLNVxt0DUOZyGBXELj1vqaAm_c1kWOW0RUqdP7QrysLqvZ2tSLUVj6acdWlUI&usqp=CAU&ec=48665698"
+
+# Display the image using Streamlit's image function
+st.write("<p style='text-align:left;'><img src='"+image_url+"' width=250 height=175></p>",unsafe_allow_html=True)
+
+st.title("Filter")
+
+# Define a function that creates a download link for a DataFrame
+def create_download_link(df, filename):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="{filename}.csv">Download {filename} CSV file</a>'
+    return href
+
+# Create a file uploader using Streamlit
+file = st.file_uploader(label="hello", type=["xlsx"], label_visibility="collapsed")
+
+if file is not None:
+    df = pd.read_excel(file)
+# Check if the first column contains alphanumeric values
+    if not df.iloc[:, 0].apply(lambda x: isinstance(x, (int, float, complex, str))).all():
+        st.error("Error: The first column must contain alphanumeric values.")
+        st.stop()
+
+    # Check for invalid values in each column, except the first column
+    error_list = []
+    for col in df.columns[1:]:
+        if not pd.to_numeric(df[col], errors='coerce').notnull().all():
+            error_list.append(col)
+
+    # Check the number of columns with errors
+    if len(error_list) != 6:
+        st.error("Error: The number of columns with alphanumerical values should be 6.")
+        st.stop()
+
+    # Check for non-alphanumeric values in each column with errors
+    for col in error_list:
+        if not df[col].apply(lambda x: isinstance(x, (int, float, complex, str))).all():
+            st.error(f"Error: Column '{col}' contains non-alphanumeric values.")
+            st.stop()
+
+    # Check if all values in every other column after the last column with errors are positive
+    last_column_name = error_list[-1]
+    last_column_index = pd.Index(df.columns).get_loc(last_column_name)
+    for i in range(last_column_index + 2, len(df.columns), 2):
+        if (df.iloc[:, i] > 0).all():
+            continue
+        else:
+            st.error(f"Error: Not all values in column '{df.columns[i]}' are positive.")
+            st.stop()
+
+    st.success("All checks passed successfully.")
+    # Allow the user to select a column to sort by
+    sort_columns = st.multiselect("Select columns to sort by:", df.columns)
+
+    # Allow the user to choose the sorting order
+    sort_order = st.radio("Select sorting order:", ("Ascending", "Descending"))
+
+    # Sort the DataFrame based on the selected column and sorting order
+    if sort_order == "Ascending":
+        sorted_df = df.sort_values(by=sort_columns)
+    else:
+        sorted_df = df.sort_values(by=sort_columns, ascending=False)
+
+    
+    gb = GridOptionsBuilder.from_dataframe(sorted_df)
+    gridOptions = gb.build()
+    
+    return_mode_value = DataReturnMode.__members__['FILTERED_AND_SORTED']
+    update_mode_value = GridUpdateMode.__members__['GRID_CHANGED']
+
+    #Display the grid
+    st.header("Streamlit Ag-Grid")
+
+    grid_response = AgGrid(
+        sorted_df, 
+        gridOptions=gridOptions,
+        height=400, 
+        width='100%',
+        data_return_mode=return_mode_value, 
+        update_mode=update_mode_value,
+        # fit_columns_on_grid_load=fit_columns_on_grid_load,
+        )
+
+    sorted_df = grid_response['data']
+    selected = grid_response['selected_rows']
+    selected_df = pd.DataFrame(selected).apply(pd.to_numeric, errors='coerce')
+    
+    # st.subheader("Returned grid data:") 
+    # st.markdown(grid_response['data'].to_html(), unsafe_allow_html=True)
+    
+    # Add a download button to the Streamlit app
+    if st.button('Download filtered data'):
+        filename = "filtered_data"
+        csv = grid_response['data'].to_csv(index=False)
+        b64 = base64.b64encode(csv.encode()).decode()
+        href = f'<a href="data:file/csv;base64,{b64}" download="{filename}.csv">Download {filename} CSV file</a>'
+        st.markdown(href, unsafe_allow_html=True)
