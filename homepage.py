46493a8194de51ae37036a46b6e8f8b50a01e386
madavanvasudevan <133659350+madavanvasudevan@users.noreply.github.com>
2023-05-19 19:25:01 +0530

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -1,2 +1,16 @@
+import base64
 import streamlit as st
 
+@st.cache_data()
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+img = get_img_as_base64("image.jpg")
+
+# URL of the image from the web
+image_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREh-y7VJtrA03RIlxLNVxt0DUOZyGBXELj1vqaAm_c1kWOW0RUqdP7QrysLqvZ2tSLUVj6acdWlUI&usqp=CAU&ec=48665698"
+
+# Display the image using Streamlit's image function
+st.write("<p style='text-align:right;'><img src='"+image_url+"' width=250 height=175></p>",unsafe_allow_html=True)
