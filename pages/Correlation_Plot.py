d6c33ce980554675c52c3cdc534f18d2e4cc4d3b
madavanvasudevan <133659350+madavanvasudevan@users.noreply.github.com>
2023-07-25 18:14:26 +0530

    Update Correlation_Plot.py

--- pages/Correlation_Plot.py
+++ pages/Correlation_Plot.py
@@ -37,8 +37,8 @@ def download_excel(data):
             return href
 # Create a file uploader using Streamlit
 file = st.file_uploader(label="hello", type=["xlsx"], label_visibility="collapsed")
-# st.write("[Sample-Input](https://docs.google.com/spreadsheets/d/1pQP-InV1VBTYVvQaYxak26_5Tm15bKI4/edit?usp=share_link&ouid=103232618408666892680&rtpof=true&sd=true)")
-# st.write("[Sample-Output](https://drive.google.com/file/d/1wiWWTo6OK2qy75tnD7WQb6GooGXKWVCq/view?usp=share_link)")
+st.write("[Sample-Input](https://docs.google.com/spreadsheets/d/1b28qMSwhqn_1Ju-KrQxfbESvHZVBUV2x/edit?usp=sharing&ouid=103232618408666892680&rtpof=true&sd=true)")
+st.write("[Sample-Output](https://docs.google.com/spreadsheets/d/11k04u5JeW_fkgZE7AWRzOfPSZG6lCBrs/edit?usp=sharing&ouid=103232618408666892680&rtpof=true&sd=true)")
 if file is not None:
     try:
         df = pd.read_excel(file)
