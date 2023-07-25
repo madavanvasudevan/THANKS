7310d27a470507515ded7cf02b622079c126aa06
madavanvasudevan <133659350+madavanvasudevan@users.noreply.github.com>
2023-07-25 18:16:25 +0530

    Update Heatmap.py

--- pages/Heatmap.py
+++ pages/Heatmap.py
@@ -37,8 +37,8 @@ def download_excel(data):
             return href
 # Create a file uploader using Streamlit
 file = st.file_uploader(label="hello", type=["xlsx"], label_visibility="collapsed")
-# st.write("[Sample-Input](https://docs.google.com/spreadsheets/d/1pQP-InV1VBTYVvQaYxak26_5Tm15bKI4/edit?usp=share_link&ouid=103232618408666892680&rtpof=true&sd=true)")
-# st.write("[Sample-Output](https://drive.google.com/file/d/1wiWWTo6OK2qy75tnD7WQb6GooGXKWVCq/view?usp=share_link)")
+st.write("[Sample-Input](https://docs.google.com/spreadsheets/d/18GRS4nCIa0gtWJPO-fRSDVU-y81V1_q_/edit?usp=sharing&ouid=103232618408666892680&rtpof=true&sd=true)")
+st.write("[Sample-Output](https://docs.google.com/spreadsheets/d/1ePqmO6YCP9UYO-cwt-3F7rVm6mkXboQp/edit?usp=sharing&ouid=103232618408666892680&rtpof=true&sd=true)")
 if file is not None:
     try:
         df = pd.read_excel(file)
