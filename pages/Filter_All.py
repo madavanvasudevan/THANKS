b91ca843cdc1dc150911264a8476a60e47e5c035
madavanvasudevan <133659350+madavanvasudevan@users.noreply.github.com>
2023-07-27 16:33:14 +0530

    Update Filter_All.py

--- pages/Filter_All.py
+++ pages/Filter_All.py
@@ -5,7 +5,6 @@ from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 
 st.set_page_config(layout="wide")
 
-
 @st.cache_data()
 def get_img_as_base64(file):
     with open(file, "rb") as f:
@@ -37,39 +36,6 @@ st.write("[Sample-Input](https://docs.google.com/spreadsheets/d/1pQP-InV1VBTYVvQ
 st.write("[Sample-Output](https://drive.google.com/file/d/1wiWWTo6OK2qy75tnD7WQb6GooGXKWVCq/view?usp=share_link)")
 if file is not None:
     df = pd.read_excel(file)
-# Check if the first column contains alphanumeric values
-    # if not df.iloc[:, 0].apply(lambda x: isinstance(x, (int, float, complex, str))).all():
-    #     st.error("Error: The first column must contain alphanumeric values.")
-    #     st.stop()
-
-    # # Check for invalid values in each column, except the first column
-    # error_list = []
-    # for col in df.columns[1:]:
-    #     if not pd.to_numeric(df[col], errors='coerce').notnull().all():
-    #         error_list.append(col)
-
-    # # Check the number of columns with errors
-    # if len(error_list) != 6:
-    #     st.error("Error: The number of columns with alphanumerical values should be 6.")
-    #     st.stop()
-
-    # # Check for non-alphanumeric values in each column with errors
-    # for col in error_list:
-    #     if not df[col].apply(lambda x: isinstance(x, (int, float, complex, str))).all():
-    #         st.error(f"Error: Column '{col}' contains non-alphanumeric values.")
-    #         st.stop()
-
-    # # Check if all values in every other column after the last column with errors are positive
-    # last_column_name = error_list[-1]
-    # last_column_index = pd.Index(df.columns).get_loc(last_column_name)
-    # for i in range(last_column_index + 2, len(df.columns), 2):
-    #     if (df.iloc[:, i] >= 0).all():
-    #         continue
-    #     else:
-    #         st.error(f"Error: Not all values in column '{df.columns[i]}' are positive.")
-    #         st.stop()
-
-    # st.success("All checks passed successfully.")
     # Allow the user to select a column to sort by
     sort_columns = st.multiselect("Select columns to sort by:", df.columns)
 
@@ -95,18 +61,13 @@ if file is not None:
         sorted_df, 
         gridOptions=gridOptions,
         height=400, 
-        width='100%',
         data_return_mode=return_mode_value, 
-        update_mode=update_mode_value,
-        fit_columns_on_grid_load=fit_columns_on_grid_load,
+        update_mode=update_mode_value
         )
 
     sorted_df = grid_response['data']
     selected = grid_response['selected_rows']
     selected_df = pd.DataFrame(selected).apply(pd.to_numeric, errors='coerce')
-
-    # st.subheader("Returned grid data:") 
-    # st.markdown(grid_response['data'].to_html(), unsafe_allow_html=True)
 
     # Add a download button to the Streamlit app
     if st.button('Proceed to Download'):
