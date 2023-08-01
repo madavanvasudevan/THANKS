2e9e3f4ac7c222a72e0ce777536856a391cb535a
madavanvasudevan <133659350+madavanvasudevan@users.noreply.github.com>
2023-08-01 18:35:04 +0530

    Update Path_Plot.py

--- pages/Path_Plot.py
+++ pages/Path_Plot.py
@@ -19,7 +19,7 @@ image_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREh-y7VJtrA03
 # Display the image using Streamlit's image function
 st.write("<p style='text-align:right;'><img src='"+image_url+"' width=250 height=175></p>",unsafe_allow_html=True)
 
-st.title("Path")
+st.title("Path_Plot")
         
 st.subheader("Upload a file")
 # Define a function that creates a download link for a DataFrame
