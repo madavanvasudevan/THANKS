8951e02ecf5fce7060c6113819ab6fc067c38654
madavanvasudevan <133659350+madavanvasudevan@users.noreply.github.com>
2023-08-01 18:21:10 +0530

    Update and rename Vlookup.py to 06Vlookup.py

--- pages/06Vlookup.py
+++ pages/06Vlookup.py
@@ -17,7 +17,7 @@ image_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREh-y7VJtrA03
 # Display the image using Streamlit's image function
 st.write("<p style='text-align:right;'><img src='"+image_url+"' width=250 height=175></p>",unsafe_allow_html=True)
 
-st.title("Vlookup")
+st.title("Vlookup_App")
         
 st.subheader("Upload your files")
 
