9b4ddfd91235281d5e5d701667dc53b4ecd38e9a
madavanvasudevan <133659350+madavanvasudevan@users.noreply.github.com>
2023-08-01 18:31:38 +0530

    Update 03Column_Seprator.py

--- pages/03Column_Seprator.py
+++ pages/03Column_Seprator.py
@@ -19,7 +19,7 @@ image_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREh-y7VJtrA03
 # Display the image using Streamlit's image function
 st.write("<p style='text-align:right;'><img src='"+image_url+"' width=250 height=175></p>",unsafe_allow_html=True)
 
-st.title("Column Separator")
+st.title("Column_Separator")
 
 st.subheader("Upload a file")
 
