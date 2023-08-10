530fd6314e3b2646f377a34e78baa25f7c0ebb3e
madavanvasudevan <133659350+madavanvasudevan@users.noreply.github.com>
2023-08-10 17:28:55 +0530

    Update 04Kegg_Annotation.py

--- pages/04Kegg_Annotation.py
+++ pages/04Kegg_Annotation.py
@@ -11,7 +11,7 @@ st.set_page_config(layout="wide")
 image_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREh-y7VJtrA03RIlxLNVxt0DUOZyGBXELj1vqaAm_c1kWOW0RUqdP7QrysLqvZ2tSLUVj6acdWlUI&usqp=CAU&ec=48665698"
 
 # Display the image using Streamlit's image function
-st.write("<p style='text-align:right;'><img src='"+image_url+"' width=250 height=175></p>",unsafe_allow_html=True)
+st.write("<p style='text-align:right;'><img src='"+image_url+"' width=250 height=150></p>",unsafe_allow_html=True)
 
 # Title and file uploader
 st.title("KEGG Annotation")
