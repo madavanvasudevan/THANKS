d996035cac2e702febf70324d96e56562c826142
madavanvasudevan <133659350+madavanvasudevan@users.noreply.github.com>
2023-08-10 16:35:35 +0530

    Update 07SNP_Heatmap.py

--- pages/07SNP_Heatmap.py
+++ pages/07SNP_Heatmap.py
@@ -4,13 +4,6 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-def get_img_as_base64(file):
-    with open(file, "rb") as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-img = get_img_as_base64("image.jpg")
-
 # URL of the image from the web
 image_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREh-y7VJtrA03RIlxLNVxt0DUOZyGBXELj1vqaAm_c1kWOW0RUqdP7QrysLqvZ2tSLUVj6acdWlUI&usqp=CAU&ec=48665698"
 
