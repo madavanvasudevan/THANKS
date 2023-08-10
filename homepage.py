2be57d4c9ef41037b96a5e99dfea46335bb161a7
madavanvasudevan <133659350+madavanvasudevan@users.noreply.github.com>
2023-08-10 18:23:30 +0530

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -17,7 +17,7 @@ background-color: rgba(0,0,0,0);
 st.markdown(page_bg_img, unsafe_allow_html=True)
 col1, col2 = st.columns(2)
 with col1:
-  st.header('**TH**eomics **AN**alytics **K**it **S**cience', anchor=None)
+  st.markdown('**TH**eomics **AN**alytics **K**it **S**cience', anchor=None)
 with col2:
   # URL of the image from the web
   image_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREh-y7VJtrA03RIlxLNVxt0DUOZyGBXELj1vqaAm_c1kWOW0RUqdP7QrysLqvZ2tSLUVj6acdWlUI&usqp=CAU&ec=48665698"
