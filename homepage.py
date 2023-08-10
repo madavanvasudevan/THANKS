c747d3cede9f9d1665102d2269bb6399fa808049
madavanvasudevan <133659350+madavanvasudevan@users.noreply.github.com>
2023-08-10 17:45:37 +0530

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -23,7 +23,7 @@ image_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREh-y7VJtrA03
 st.write("<p style='text-align:right;'><img src='"+image_url+"' width=250 height=150></p>",unsafe_allow_html=True)
 
 
-st.title("THANKS-APP",anchor='False')
+st.title("THANKS-APP",anchor ="False")
 
 st.subheader("TH-Theomics")
 st.subheader("AN-Analysis")
