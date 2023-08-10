719b9152ae11541a5f6be1c9d73ffdb5e7b0c2ff
madavanvasudevan <133659350+madavanvasudevan@users.noreply.github.com>
2023-08-10 18:39:39 +0530

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -14,11 +14,28 @@ background-color: rgba(0,0,0,0);
 </style>
 """
 
+st.write(
+    """
+    <style>
+    .large-text {
+        font-size: 42px;
+    }
+    .medium-text {
+        font-size: 24px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+
 st.markdown(page_bg_img, unsafe_allow_html=True)
 col1, col2 = st.columns(2)
 with col1:
-  st.title("**TH**eomics **AN**alytics **K**it **S**cience")
-  # st.subheader("<span style='font-size: 42px;'>**TH**</span>eomics <span style='font-size: 42px;'>**AN**</span>alytics <span style='font-size: 42px;'>**K**</span>it <span style='font-size: 42px;'>**S**</span>cience", unsafe_allow_html=True)
+  st.markdown(
+      "<span class='large-text'>**TH**</span><span class='medium-text'>eomics</span><span class='large-text'>**AN**</span>class='medium-text'>alytics</span> <span class='large-text'>**K**</span>class='medium-text'>it</span> <span class='large-text'>**S**</span>class='medium-text'>cience</span>",
+      unsafe_allow_html=True
+  )
 with col2:
   # URL of the image from the web
   image_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREh-y7VJtrA03RIlxLNVxt0DUOZyGBXELj1vqaAm_c1kWOW0RUqdP7QrysLqvZ2tSLUVj6acdWlUI&usqp=CAU&ec=48665698"
