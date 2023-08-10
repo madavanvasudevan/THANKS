478ec70b7c027503e38735009d6dbdfb831ee8c9
madavanvasudevan <133659350+madavanvasudevan@users.noreply.github.com>
2023-08-10 17:12:51 +0530

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -3,6 +3,7 @@ page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] > .main {{
 background-image: url("https://images.unsplash.com/photo-1501426026826-31c667bdf23d");
+background-size: cover;
 }}
 
 </style>
