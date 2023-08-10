11f1ced79d1932e096b79a7fcf7146c5051bebdc
madavanvasudevan <133659350+madavanvasudevan@users.noreply.github.com>
2023-08-10 18:33:52 +0530

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -17,8 +17,8 @@ background-color: rgba(0,0,0,0);
 st.markdown(page_bg_img, unsafe_allow_html=True)
 col1, col2 = st.columns(2)
 with col1:
-  st.subheader("**TH**eomics **AN**alytics **K**it **S**cience")
-  st.subheader("<span style='font-size: 42px;'>**TH**</span>eomics <span style='font-size: 42px;'>**AN**</span>alytics <span style='font-size: 42px;'>**K**</span>it <span style='font-size: 42px;'>**S**</span>cience", unsafe_allow_html=True)
+  st.header("**TH**eomics **AN**alytics **K**it **S**cience")
+  # st.subheader("<span style='font-size: 42px;'>**TH**</span>eomics <span style='font-size: 42px;'>**AN**</span>alytics <span style='font-size: 42px;'>**K**</span>it <span style='font-size: 42px;'>**S**</span>cience", unsafe_allow_html=True)
 with col2:
   # URL of the image from the web
   image_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREh-y7VJtrA03RIlxLNVxt0DUOZyGBXELj1vqaAm_c1kWOW0RUqdP7QrysLqvZ2tSLUVj6acdWlUI&usqp=CAU&ec=48665698"
