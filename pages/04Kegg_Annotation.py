e84a4f11e941aba8914c0a1c6d78289ef39dc6be
madavanvasudevan <133659350+madavanvasudevan@users.noreply.github.com>
2023-08-10 16:34:33 +0530

    Update 04Kegg_Annotation.py

--- pages/04Kegg_Annotation.py
+++ pages/04Kegg_Annotation.py
@@ -7,15 +7,6 @@ from io import BytesIO
 # Set Streamlit page configuration
 st.set_page_config(layout="wide")
 
-# Define a function to convert an image file to base64
-@st.cache_data()
-def get_img_as_base64(file):
-    with open(file, "rb") as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-img = get_img_as_base64("image.jpg")
-
 # URL of the image from the web
 image_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREh-y7VJtrA03RIlxLNVxt0DUOZyGBXELj1vqaAm_c1kWOW0RUqdP7QrysLqvZ2tSLUVj6acdWlUI&usqp=CAU&ec=48665698"
 
