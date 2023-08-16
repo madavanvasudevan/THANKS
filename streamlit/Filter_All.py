a05a2af90129013f26c6febf1c03fd434069147e
madavanvasudevan <133659350+madavanvasudevan@users.noreply.github.com>
2023-08-16 19:26:26 +0530

    Add files via upload

---
+++ streamlit/Filter_All.py
@@ -0,0 +1,86 @@
+import base64
+import pandas as pd
+import streamlit as st
+from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode  
+
+custom_css = """
+<style>
+    /* Replace 'your_grid_container_id' with the actual ID of your AgGrid container */
+    #your_grid_container_id {
+        overflow-y: auto; /* You can also use 'scroll' instead of 'auto' */
+        /* Add other custom CSS styles as needed */
+    }
+</style>
+"""
+st.set_page_config(layout="wide")
+
+@st.cache_data()
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+
+img = get_img_as_base64("image.jpg")
+
+# URL of the image from the web
+image_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREh-y7VJtrA03RIlxLNVxt0DUOZyGBXELj1vqaAm_c1kWOW0RUqdP7QrysLqvZ2tSLUVj6acdWlUI&usqp=CAU&ec=48665698"
+
+# Display the image using Streamlit's image function
+st.write("<p style='text-align:right;'><img src='"+image_url+"' width=250 height=175></p>",unsafe_allow_html=True)
+
+st.title("Filter07")
+
+st.subheader("Upload your files")
+
+# Define a function that creates a download link for a DataFrame
+def create_download_link(df, filename):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="{filename}.csv">Download {filename} CSV file</a>'
+    return href
+
+# Create a file uploader using Streamlit
+file = st.file_uploader(label="hello", type=["xlsx"], label_visibility="collapsed")
+st.write("[Sample-Input](https://docs.google.com/spreadsheets/d/1pQP-InV1VBTYVvQaYxak26_5Tm15bKI4/edit?usp=share_link&ouid=103232618408666892680&rtpof=true&sd=true)")
+st.write("[Sample-Output](https://drive.google.com/file/d/1wiWWTo6OK2qy75tnD7WQb6GooGXKWVCq/view?usp=share_link)")
+if file is not None:
+    df = pd.read_excel(file)
+    # Allow the user to select a column to sort by
+    sort_columns = st.multiselect("Select columns to sort by:", df.columns)
+
+    # Allow the user to choose the sorting order
+    sort_order = st.radio("Select sorting order:", ("Ascending", "Descending"))
+
+    # Sort the DataFrame based on the selected column and sorting order
+    if sort_order == "Ascending":
+        sorted_df = df.sort_values(by=sort_columns)
+    else:
+        sorted_df = df.sort_values(by=sort_columns, ascending=False)
+    
+    gb = GridOptionsBuilder.from_dataframe(sorted_df)
+    gridOptions = gb.build()
+    gridOptions['suppressVerticalScroll'] = False
+
+    return_mode_value = DataReturnMode.__members__['FILTERED_AND_SORTED']
+    update_mode_value = GridUpdateMode.__members__['GRID_CHANGED']
+
+    #Display the grid
+    st.header("Streamlit Ag-Grid")
+
+    grid_response = AgGrid(
+        sorted_df, 
+        gridOptions=gridOptions,
+        data_return_mode=return_mode_value, 
+        update_mode=update_mode_value,
+        )
+    sorted_df = grid_response['data']
+    selected = grid_response['selected_rows']
+    selected_df = pd.DataFrame(selected).apply(pd.to_numeric, errors='coerce')
+
+    # Add a download button to the Streamlit app
+    if st.button('Proceed to Download'):
+        filename = "filtered_data"
+        csv = grid_response['data'].to_csv(index=False)
+        b64 = base64.b64encode(csv.encode()).decode()
+        href = f'<a href="data:file/csv;base64,{b64}" download="{filename}.csv">Download {filename} CSV file</a>'
+        st.markdown(href, unsafe_allow_html=True) 
---
+++ streamlit/Kegg.py
@@ -0,0 +1,78 @@
+import re
+import pandas as pd
+import streamlit as st
+import base64
+from io import BytesIO
+
+# Set Streamlit page configuration
+st.set_page_config(layout="wide")
+
+# Check if the user is logged in
+if st.session_state.get('logged_in', False):
+    # st.markdown("Welcome to the protected page!")
+    # Display the contents of the protected page
+    
+    @st.cache_data()
+    def get_img_as_base64(file):
+        with open(file, "rb") as f:
+            data = f.read()
+        return base64.b64encode(data).decode()
+
+    img = get_img_as_base64("image.jpg")
+
+    # URL of the image from the web
+    image_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREh-y7VJtrA03RIlxLNVxt0DUOZyGBXELj1vqaAm_c1kWOW0RUqdP7QrysLqvZ2tSLUVj6acdWlUI&usqp=CAU&ec=48665698"
+
+    # Display the image using Streamlit's image function
+    st.write("<p style='text-align:right;'><img src='"+image_url+"' width=250 height=175></p>",unsafe_allow_html=True)
+
+
+    # Title and file uploader
+    st.title("KEGG Annotation")
+    st.subheader("Upload a file")
+    file3 = st.file_uploader(label="Select a file", type=["txt"], label_visibility="collapsed")
+    st.write("[Sample-Input](https://drive.google.com/file/d/14jzsr3Xl6Ekfim1EtWGW5fTDDStSoAvv/view?usp=share_link)")
+    st.write("[Sample-Output](https://docs.google.com/spreadsheets/d/1cUAqGNQXzRswIsrQ5EeyRc68mVQrWJUb/edit?usp=share_link&ouid=103232618408666892680&rtpof=true&sd=true)")
+    
+    if file3 is not None:
+        # Read the uploaded file into a DataFrame
+        df = pd.read_csv(file3, header=None,delimiter='None')
+        
+        # Create an input field for the "map" data
+        map_data = st.text_input("Enter map data (e.g., map123): ")
+        if map_data:
+
+            # Escape special characters in the user input
+            escaped_map_data = re.escape(map_data)
+            # Extract the "map" data from the first column and create a new column
+            df['Map'] = df.iloc[:, 0].str.extract('(' + escaped_map_data + r'\d+ .+?)(?=\s*\()(?!.*:)')
+            df['Map'].fillna(method='ffill', inplace=True)
+
+            # Create a boolean mask based on regex pattern match in first column
+            mask = df.iloc[:, 0].str.extract('(' + escaped_map_data + r'\d+ .+)').notna().squeeze()
+
+            # Use boolean mask to select rows to be deleted and drop them
+            df.drop(df[mask].index, inplace=True)
+            
+            # Drop rows with missing values in the 'Map' column
+            df.dropna(subset=[0], inplace=True)
+
+            # Display the processed data in a table
+            st.subheader("Data Processing with Streamlit")
+            st.write("Processed Data:")
+            st.dataframe(df)
+    
+            # Create a function to download the processed data as an Excel file
+            def download_excel(data):
+                excel_file = BytesIO()
+                df.to_excel(excel_file, index=False)
+                excel_file.seek(0)
+                b64 = base64.b64encode(excel_file.read()).decode()
+                href = f'<a href="data:application/octet-stream;base64,{b64}" download="output.xlsx">Download Excel file</a>'
+                return href
+
+            # Display the download button for the Excel file
+            st.markdown(download_excel(df), unsafe_allow_html=True)
+else:
+    st.markdown("You need to log in to access this page.")
+    # Display a message or redirect the user to the login page
