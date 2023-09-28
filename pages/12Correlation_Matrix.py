d00de5834b03687c3353309a5070fc8391412b7d
madavanvasudevan <133659350+madavanvasudevan@users.noreply.github.com>
2023-09-28 17:12:20 +0530

    Update 12Correlation_Matrix.py

--- pages/12Correlation_Matrix.py
+++ pages/12Correlation_Matrix.py
@@ -115,6 +115,7 @@ def main():
         dp.read_data(uploaded_file)
         dp.do_process()
         dp.create_corr_matrix_and_plot()  # Call the new function to create and display the correlation matrix
-
+        # Add the Excel download button after processing and displaying the data
+        dp.download_excel()
 if __name__ == "__main__":
     main()
