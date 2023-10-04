3908dd1799a033da66ac29c24e01f4280a68952e
madavanvasudevan <133659350+madavanvasudevan@users.noreply.github.com>
2023-10-04 18:30:25 +0530

    Update 12Differentiate Matrix.py

--- pages/12Differentiate Matrix.py
+++ pages/12Differentiate Matrix.py
@@ -6,6 +6,12 @@ import matplotlib.pyplot as plt
 
 st.set_page_config(layout="wide")
 
+# URL of the image from the web
+image_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREh-y7VJtrA03RIlxLNVxt0DUOZyGBXELj1vqaAm_c1kWOW0RUqdP7QrysLqvZ2tSLUVj6acdWlUI&usqp=CAU&ec=48665698"
+
+# Display the image using Streamlit's image function
+st.write("<p style='text-align:right;'><img src='"+image_url+"' width=250 height=150></p>",unsafe_allow_html=True)
+
 class Gene:
     def __init__(self, name=""):
         self.gene_name = name
