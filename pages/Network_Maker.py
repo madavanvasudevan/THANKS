6aaae667c053f4bcbac1831b4674d475317af564
madavanvasudevan <133659350+madavanvasudevan@users.noreply.github.com>
2023-10-13 15:52:46 +0530

    Update Network_Maker.py

--- pages/Network_Maker.py
+++ pages/Network_Maker.py
@@ -6,8 +6,14 @@ import base64
 
 st.set_page_config(layout="wide")
 
+# URL of the image from the web
+image_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREh-y7VJtrA03RIlxLNVxt0DUOZyGBXELj1vqaAm_c1kWOW0RUqdP7QrysLqvZ2tSLUVj6acdWlUI&usqp=CAU&ec=48665698"
+
+# Display the image using Streamlit's image function
+st.write("<p style='text-align:right;'><img src='"+image_url+"' width=250 height=150></p>",unsafe_allow_html=True)
+
 # Define a Streamlit app title and description
-st.title("Gene-Process Analysis")
+st.title("Network Maker")
 st.subheader("Upload your files")
 # Function to trigger file download
 def download_file(file_path, file_name):
