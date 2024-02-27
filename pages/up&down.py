467c84946931ac6a976c0133dce2cc1d366be584
madavanvasudevan <133659350+madavanvasudevan@users.noreply.github.com>
2024-02-27 19:49:28 +0530

    Update up&down.py

--- pages/up&down.py
+++ pages/up&down.py
@@ -6,27 +6,15 @@ import plotly.graph_objects as go
 
 st.set_page_config(layout="wide")
 
-# Check if the user is logged in
-# if st.session_state.get('logged_in', False):
-#     # st.markdown("Welcome to the protected page!")
-#     # Display the contents of the protected page
-
-def get_img_as_base64(file):
-    with open(file, "rb") as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-img = get_img_as_base64("image.jpg")
-
 # URL of the image from the web
 image_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREh-y7VJtrA03RIlxLNVxt0DUOZyGBXELj1vqaAm_c1kWOW0RUqdP7QrysLqvZ2tSLUVj6acdWlUI&usqp=CAU&ec=48665698"
 
 # Display the image using Streamlit's image function
-st.write("<p style='text-align:right;'><img src='"+image_url+"' width=250 height=175></p>",unsafe_allow_html=True)
+st.write("<p style='text-align:right;'><img src='"+image_url+"' width=250 height=150></p>",unsafe_allow_html=True)
 
-st.title("GeneUp&Down")
+st.title("↑&↓Gene_Plot")
         
-st.subheader("Upload your files")
+st.subheader("Upload a file")
 
 # Define a function that creates a download link for a DataFrame
 def download_excel(data):
@@ -37,9 +25,9 @@ def download_excel(data):
             href = f'<a href="data:application/octet-stream;base64,{b64}" download="output.xlsx">Download Excel file</a>'
             return href
 # Create a file uploader using Streamlit
-file = st.file_uploader(label="hello", type=["xlsx"], label_visibility="collapsed")
-# st.write("[Sample-Input](https://docs.google.com/spreadsheets/d/1pQP-InV1VBTYVvQaYxak26_5Tm15bKI4/edit?usp=share_link&ouid=103232618408666892680&rtpof=true&sd=true)")
-# st.write("[Sample-Output](https://drive.google.com/file/d/1wiWWTo6OK2qy75tnD7WQb6GooGXKWVCq/view?usp=share_link)")
+file = st.file_uploader(label="hello", type=["xlsx"], label_visibility="collapsed",key="UP&DOWN")
+st.write("[Sample-Input](https://docs.google.com/spreadsheets/d/1dZbmL27Wg6dWfYmpKFJDFqHg6EkNb4yA/edit?usp=sharing&ouid=103232618408666892680&rtpof=true&sd=true)")
+st.write("[Sample-Output](https://docs.google.com/spreadsheets/d/1I3HsN0pvUrluHKl20TDry6TCA5tRA_oj/edit?usp=sharing&ouid=103232618408666892680&rtpof=true&sd=true)")
 if file is not None:
     try:
         df = pd.read_excel(file)
