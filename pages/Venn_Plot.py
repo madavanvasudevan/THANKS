3f41562fadb189abcbd7fe145791b1bb32b2f409
madavanvasudevan <133659350+madavanvasudevan@users.noreply.github.com>
2024-04-16 17:24:43 +0530

    Update Venn_Plot.py

--- pages/Venn_Plot.py
+++ pages/Venn_Plot.py
@@ -17,9 +17,7 @@ image_url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREh-y7VJtrA03
 st.write("<p style='text-align:right;'><img src='"+image_url+"' width=250 height=175></p>",unsafe_allow_html=True)
 
 # Web App Title
-st.markdown('''
-# **Venn_Plot**
-''')
+st.title("Venn_Plot")
 
 st.subheader("Upload your files")
 
